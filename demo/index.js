a648a0d4cd3a59483e8917594c4c157326f11417
Haochong Guo <haochong@gmail.com>
2016-07-19 16:52:06 +0800

    nodejs server for demo

---
+++ demo/index.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var fs=require('fs');
+const PORT=3000;
+
+function handleRequest(request, response){
+
+    var file = './demo/index.html';
+    if(request.url.indexOf('/dist/') > -1) {
+        file = './'+request.url;
+    }
+    fs.readFile(file,function(error,data){
+        if(error){
+            response.writeHead(404,{"Content-type":"text/plain"});
+            response.end("Sorry the page was not found");
+        }else{
+            var dotoffset = request.url.lastIndexOf('.');
+            var mimetype = dotoffset == -1
+                ? 'text/html'
+                : {
+                '.html' : 'text/html',
+                '.ico' : 'image/x-icon',
+                '.jpg' : 'image/jpeg',
+                '.png' : 'image/png',
+                '.gif' : 'image/gif',
+                '.css' : 'text/css',
+                '.js' : 'text/javascript'
+                }[ request.url.substr(dotoffset) ];
+            response.setHeader('Content-type' , mimetype);
+            response.end(data);
+        }
+    });
+
+    //response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
